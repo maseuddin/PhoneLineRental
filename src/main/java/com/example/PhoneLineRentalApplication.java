0e18576077eff32b0c8fc20e8920bbb0231e1242
Masum Uddin <masumuddin@Masums-Air.cust.communityfibre.co.uk>
2022-10-26 12:52:07 +0100

    first commit

---
+++ src/main/java/com/example/PhoneLineRentalApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneLineRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneLineRentalApplication.class, args);
+	}
+
+}
